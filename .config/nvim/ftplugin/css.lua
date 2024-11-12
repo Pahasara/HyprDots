eb353a52950992a99dea705c8a7658ee34151fac
pahasara <pahasarapro@gmail.com>
2024-11-12 17:16:45 +0530

    Update neovim config

---
+++ .config/nvim/ftplugin/css.lua
@@ -0,0 +1,5 @@
+local opt = vim.opt -- for conciseness
+
+-- tabs & indentation
+opt.tabstop = 2 -- number of visual spaces per TAB
+opt.shiftwidth = 2 -- insert 4 spaces on a tab
--- .config/nvim/lua/core/lazy.lua
+++ .config/nvim/lua/core/lazy.lua
@@ -11,22 +11,28 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+-- Helper function to dynamically import all Lua files and directories within a given directory
+local function import_all_plugins(directory)
+  local imports = {}
+  for _, file in ipairs(vim.fn.glob(directory .. '/**/*.lua', true, true)) do
+    -- Convert the path into a Lua module format
+    local module_name = file:match('nvim/lua/(.*).lua$'):gsub('/', '.')
+    table.insert(imports, { import = module_name })
+  end
+  return imports
+end
 
-  { import = 'plugins.ui' },
-  { import = 'plugins.editor' },
-  { import = 'plugins.utils' },
-  { import = 'plugins.lsp' },
-  { import = 'plugins.debugger' },
-}, {
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-  },
-})
+require('lazy').setup(
+  vim.list_extend({}, import_all_plugins(vim.fn.stdpath 'config' .. '/lua/plugins')), -- Automatically import everything under plugins
+  {
+    checker = {
+      enabled = true,
+      notify = false,
+    },
+    change_detection = {
+      notify = false,
+    },
+  }
+)
 
 -- vim: ts=2 sts=2 sw=2 et
