e4f4180dfe75816d4354926958df8191210d9280
DvNET <46932317+Pahasara@users.noreply.github.com>
2024-12-20 20:36:12 +0530

    Update yazi configs

--- .config/yazi/plugins/mediainfo.yazi/README.md
+++ .config/yazi/plugins/mediainfo.yazi/README.md
@@ -1,22 +1,29 @@
 # mediainfo.yazi
 
+<!--toc:start-->
+
+- [mediainfo.yazi](#mediainfoyazi)
+  - [Installation](#installation)
+  <!--toc:end-->
+
 This is a Yazi plugin for previewing media files. The preview shows thumbnail
-using `ffmpegthumbnailer` if available and media metadata using `mediainfo`.
+using `ffmpeg` if available and media metadata using `mediainfo`.
+Only for yazi >= 0.4
 
 ## Installation
 
 Install the plugin:
 
-```
-ya pack -a Ape/mediainfo
+```bash
+ya pack -a boydaihungst/mediainfo
 ```
 
 Create `~/.config/yazi/yazi.toml` and add:
 
-```
+```toml
 [plugin]
 prepend_previewers = [
     { mime = "{image,audio,video}/*", run = "mediainfo"},
-    { mime = "application/x-subrip", run = "mediainfo"},
+    { mime = "application/subrip", run = "mediainfo"},
 ]
 ```
--- .config/yazi/plugins/mediainfo.yazi/init.lua
+++ .config/yazi/plugins/mediainfo.yazi/init.lua
@@ -1,3 +1,4 @@
+local show_image_preview = true
 local skip_labels = {
 	["Complete name"] = true,
 	["CompleteName_Last"] = true,
@@ -9,103 +10,111 @@ local skip_labels = {
 }
 
 local M = {}
+local suffix = "_mediainfo"
 
-function M:peek()
-	local image_height = 0
+local function read_mediainfo_cached_file(file_path)
+	-- Open the file in read mode
+	local file = io.open(file_path, "r")
 
-	if self:preload() == 1 then
-		local cache = ya.file_cache(self)
-		if cache and fs.cha(cache).length > 0 then
-			image_height = ya.image_show(cache, self.area).h
-		end
+	if file then
+		-- Read the entire file content
+		local content = file:read("*all")
+		file:close()
+		return content
+	end
+end
+
+function M:peek(job)
+	local start, cache_img_url = os.clock(), ya.file_cache(job)
+	if not cache_img_url or self:preload(job) ~= 1 then
+		return
 	end
 
-	local cmd = "mediainfo"
-	local output, code = Command(cmd)
-		:args({ tostring(self.file.url) })
-		:stdout(Command.PIPED)
-		:output()
+	local cache_img_url_no_skip = ya.file_cache({ file = job.file, skip = 0 })
+	local cache_mediainfo_path = tostring(cache_img_url_no_skip) .. suffix
+	ya.sleep(math.max(0, PREVIEW.image_delay / 1000 + start - os.clock()))
+	local output = read_mediainfo_cached_file(cache_mediainfo_path)
 
 	local lines = {}
 
 	if output then
 		local i = 0
-		for str in output.stdout:gmatch("[^\n]*") do
+		for str in output:gmatch("[^\n]*") do
 			local label, value = str:match("(.*[^ ])  +: (.*)")
 			local line
 
 			if label then
 				if not skip_labels[label] then
 					line = ui.Line({
-						ui.Span(label .. ": "):bold(),
-						ui.Span(value),
+						ui.Span(label .. ": "):style(ui.Style():bold()),
+						ui.Span(value):style(ui.Style():fg("blue")),
 					})
 				end
 			elseif str ~= "General" then
-				line = ui.Line({ ui.Span(str):underline() })
+				line = ui.Line({ ui.Span(str):style(ui.Style():fg("green")) })
 			end
 
 			if line then
-				if i >= self.skip then
+				if i >= job.skip then
 					table.insert(lines, line)
 				end
 
-				local max_width = math.max(1, self.area.w - 3)
+				local max_width = math.max(1, job.area.w - 3)
 				i = i + math.max(1, math.ceil(line:width() / max_width))
 			end
 		end
-	else
-		local error = string.format("Spawn `%s` command returns %s", cmd, code)
-		table.insert(lines, ui.Line(error))
 	end
 
-	ya.preview_widgets(self, {
-		ui.Paragraph(
-			ui.Rect({
-				x = self.area.x,
-				y = self.area.y + image_height,
-				w = self.area.w,
-				h = self.area.h - image_height,
-			}),
-			lines
-		):wrap(ui.Paragraph.WRAP),
+	local rendered_img_rect
+	if show_image_preview then
+		rendered_img_rect = ya.image_show(cache_img_url, job.area)
+	end
+
+	local image_height = rendered_img_rect and rendered_img_rect.h or 0
+	ya.preview_widgets(job, {
+		ui.Text(lines)
+			:area(ui.Rect({
+				x = job.area.x,
+				y = job.area.y + image_height,
+				w = job.area.w,
+				h = job.area.h - image_height,
+			}))
+			:wrap(ui.Text.WRAP),
 	})
 end
 
-function M:seek(units)
+function M:seek(job)
 	local h = cx.active.current.hovered
-	if h and h.url == self.file.url then
-		local step = math.floor(units * self.area.h / 10)
+	if h and h.url == job.file.url then
 		ya.manager_emit("peek", {
-			math.max(0, cx.active.preview.skip + step),
-			only_if = self.file.url,
+			math.max(0, cx.active.preview.skip + job.units),
+			only_if = job.file.url,
 		})
 	end
 end
 
-function M:preload()
-	local cache = ya.file_cache(self)
-	if not cache or fs.cha(cache) then
+function M:preload(job)
+	local video = require("video")
+	video = ya.dict_merge(video, { skip = job.skip, file = job.file })
+	local cache_img_status = video:preload(job)
+	if cache_img_status ~= 1 then
+		return cache_img_status
+	end
+
+	local cache_img_url_no_skip = ya.file_cache({ file = job.file, skip = 0 })
+	local cache_mediainfo_url = Url(tostring(cache_img_url_no_skip) .. suffix)
+
+	local cha = fs.cha(cache_mediainfo_url)
+	if cha and cha.len > 1000 then
 		return 1
 	end
+	local cmd = "mediainfo"
+	local output, _ = Command(cmd):args({ tostring(job.file.url) }):stdout(Command.PIPED):output()
 
-	local cmd = "ffmpegthumbnailer"
-	local child, code = Command(cmd):args({
-		"-q", "6",
-		"-c", "jpeg",
-		"-i", tostring(self.file.url),
-		"-o", tostring(cache),
-		"-t", "5",
-		"-s", tostring(PREVIEW.max_width),
-	}):spawn()
-
-	if not child then
-		ya.err(string.format("spawn `%s` command returns %s", cmd, code))
-		return 0
-	end
-
-	local status = child:wait()
-	return status and status.success and 1 or 2
+	return fs.write(
+		cache_mediainfo_url,
+		output and output.stdout or string.format("Failed to start `%s`, Do you have `%s` installed?", cmd, cmd)
+	) and 1 or 2
 end
 
 return M
